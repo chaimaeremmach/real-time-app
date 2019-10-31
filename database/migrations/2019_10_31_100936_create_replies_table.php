61e3e200ea27dba1eb8319251903f0837e640aac
chaimae <remmach.chaimae@gmail.com>
2019-10-31 13:10:07 +0100

    Creating Migration , Model , Controller , Factories

---
+++ database/migrations/2019_10_31_100936_create_replies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->text('body');
+            $table->integer('question_id')->unsigned();
+            $table->text('user_id');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
