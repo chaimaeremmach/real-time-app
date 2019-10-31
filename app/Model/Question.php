036101a3bd5e43d013550e9fc40830b0b7ae330c
chaimae <remmach.chaimae@gmail.com>
2019-10-31 13:51:29 +0100

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    //
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function replies(){
+        return $this->hasMany(Reply::class);
+    }
 }
